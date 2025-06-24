0715bb4f03204b09dc7d642697604d425817e0e7
Aeron <dequitoaeron353@gmail.com>
2025-06-24 18:57:39 +0800

    constraint the spcaship so it doesnt go outside the  box

--- spaceship.py
+++ spaceship.py
@@ -1,22 +1,27 @@
 import pygame
 class Spaceship(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_heigth):
-        super().__init__()
-        self.screen_width = screen_width
-        self.screen_heigth = screen_heigth
-        self.image = pygame.image.load("Graphics/spaceship.png")
-        self.rect = self.image.get_rect(midbottom = (self.screen_width/2, self.screen_heigth))      
-        self.speed = 6
-    
-    def get_user_input(self):
-        keys = pygame.key.get_pressed()
+	def __init__(self, screen_width, screen_heigth):
+		super().__init__()
+		self.screen_width = screen_width
+		self.screen_heigth = screen_heigth
+		self.image = pygame.image.load("Graphics/spaceship.png")
+		self.rect = self.image.get_rect(midbottom = (self.screen_width/2, self.screen_heigth))      
+		self.speed = 6
+	
+	def get_user_input(self):
+		keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
-                    
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
+		if keys[pygame.K_RIGHT]:
+			self.rect.x += self.speed
+					
+		if keys[pygame.K_LEFT]:
+			self.rect.x -= self.speed
 
-    def update(self):
-	    self.get_user_input()
-		
+	def update(self):
+		self.get_user_input()
+		self.constrain_movement()
+	def constrain_movement(self):
+		if self.rect.right > self.screen_width:
+			self.rect.right = self.screen_width
+		if self.rect.left < 0:
+			self.rect.left = 0	

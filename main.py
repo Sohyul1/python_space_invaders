52fd41996593e4eab8be3d92265ac1389e52fcd4
Aeron <dequitoaeron353@gmail.com>
2025-06-24 17:55:58 +0800

    Basic setup of the game such as screen size, screen title and the clock

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init
+
+screen_width = 800
+screen_height = 700
+
+screen  = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pyhton Space Invaders")
+
+clock =  pygame.time.Clock()

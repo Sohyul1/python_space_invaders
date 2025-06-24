ae21e1508c922dad922439c8df54396239953e40
Aeron <dequitoaeron353@gmail.com>
2025-06-24 18:40:00 +0800

    made an  object to use as a position of the spaceship

--- spaceship.py
+++ spaceship.py
@@ -1,6 +1,8 @@
 import pygame
 class Spaceship(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, screen_width, screen_heigth):
         super().__init__()
+        self.screen_width = screen_width
+        self.screen_heigth = screen_heigth
         self.image = pygame.image.load("Graphics/spaceship.png")
-        self.rect = self.image.get_rect(midbottom = (100, 100))      
+        self.rect = self.image.get_rect(midbottom = (self.screen_width/2, self.screen_heigth))      

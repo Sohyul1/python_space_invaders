5c4d35bf480b9ca335142f46c2e06a8bdb4ed775
Aeron <dequitoaeron353@gmail.com>
2025-06-24 18:31:09 +0800

    fixed the typo in midebottom to midbottom

--- spaceship.py
+++ spaceship.py
@@ -3,4 +3,4 @@ class Spaceship(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("Graphics/spaceship.png")
-        self.rect = self.image.get_rect(midebottom = (100, 100))      
+        self.rect = self.image.get_rect(midbottom = (100, 100))      

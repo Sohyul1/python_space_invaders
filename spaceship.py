ec925c737c5016bb002a62a67f9acea4f9bd7e1b
Aeron <dequitoaeron353@gmail.com>
2025-06-24 18:51:08 +0800

    made the spaceship move and its speed using keyboard

--- spaceship.py
+++ spaceship.py
@@ -6,3 +6,17 @@ class Spaceship(pygame.sprite.Sprite):
         self.screen_heigth = screen_heigth
         self.image = pygame.image.load("Graphics/spaceship.png")
         self.rect = self.image.get_rect(midbottom = (self.screen_width/2, self.screen_heigth))      
+        self.speed = 6
+    
+    def get_user_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+                    
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+
+    def update(self):
+	    self.get_user_input()
+		

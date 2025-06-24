96efcbe70adb3cd3bc691fd503ca6873c5679d0d
Aeron <dequitoaeron353@gmail.com>
2025-06-24 18:07:07 +0800

    added  the loop for checking events, updating positon of game objects, and drawing the game objects in their new position

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import pygame
+import pygame, sys
 
 pygame.init
 
 screen_width = 800
 screen_height = 700
 
+grey = (29,29,27)
+
 screen  = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pyhton Space Invaders")
 
 clock =  pygame.time.Clock()
+
+while True:
+    for event in  pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+        # Drawing 
+        screen.fill(grey)
+        
+        pygame.display.update()
+        clock.tick(60)

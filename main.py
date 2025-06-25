fa5b6969ee6435ad6a1b6c953a4bbf669259e4bd
Aeron <dequitoaeron353@gmail.com>
2025-06-26 00:10:19 +0800

    updated the aliens movement and shooting, also made it shooting interval as 1 hit per 300m milisecond

--- main.py
+++ main.py
@@ -15,21 +15,29 @@ clock =  pygame.time.Clock()
 
 game = Game(screen_width, screen_height,)
 
+shoot_laser = pygame.USEREVENT
+pygame.time.set_timer(shoot_laser, 300)
+
 while True: 
     for event in  pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        if event.type == shoot_laser:
+            game.alien_shoot_laser()
         # Updating
         game.spaceship_group.update()
-        
+        game.move_aliens()
+        game.alien_lasers_group.update()
+
         # Drawing 
         screen.fill(grey)
         game.spaceship_group.draw(screen)
         game.spaceship_group.sprite.laser_group.draw(screen)
         for obstacle in game.obstacles:
             obstacle.blocks_group.draw(screen)
+        game.aliens_group.draw(screen)
+        game.alien_lasers_group.draw(screen)
         
         pygame.display.update()
         clock.tick(60)

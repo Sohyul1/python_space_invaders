4aba8f67302c67afc5b30ae499546a1ee55c835d
Aeron <dequitoaeron353@gmail.com>
2025-06-26 04:14:59 +0800

    added a bbg music and explosion sound effects

--- game.py
+++ game.py
@@ -24,6 +24,9 @@ class Game:
 		self.score = 0 
 		self.highscore = 0
 		self.load_highscore
+		pygame.mixer.music.load("SFX/music.ogg")
+		pygame.mixer.music.play(-1)
+		self.explosion_sound = pygame.mixer.Sound("SFX/explosion.ogg")
 
 	def create_obstacles(self):
 		obstacle_width = len(grid[0]) * 3
@@ -81,6 +84,7 @@ class Game:
 				aliens_hit = pygame.sprite.spritecollide(laser_sprite, self.aliens_group, True)
 					
 				if aliens_hit:
+					self.explosion_sound.play()
 					for alien in aliens_hit:
 						self.score += alien.type * 100
 						self.check_for_highscore()
@@ -88,6 +92,7 @@ class Game:
 
 				if pygame.sprite.spritecollide(laser_sprite, self.mystery_ship_group, True):
 					self.score += 500
+					self.explosion_sound.play()
 					self.check_for_highscore()
 					laser_sprite.kill()
 

5cf4adc83a3cd74e44949a1b427beec2c93239ff
Aeron <dequitoaeron353@gmail.com>
2025-06-25 23:11:45 +0800

    made the obstacle in front of the spaceship

--- game.py
+++ game.py
@@ -1,10 +1,22 @@
 import pygame
 from spaceship import Spaceship
+from obstacle import Obstacle
+from obstacle import grid
 
 class Game:
-	def __init__(self, screen_width, screen_height, offset):
+	def __init__(self, screen_width, screen_height):
 		self.screen_width = screen_width
 		self.screen_height = screen_height
-		self.offset = offset
 		self.spaceship_group = pygame.sprite.GroupSingle()
-		self.spaceship_group.add(Spaceship(self.screen_width, self.screen_height, self.offset))
+		self.spaceship_group.add(Spaceship(self.screen_width, self.screen_height))
+		self.obstacles = self.create_obstacles()
+		
+	def create_obstacles(self):
+		obstacle_width = len(grid[0]) * 3
+		gap = (self.screen_width - (4 * obstacle_width))/5
+		obstacles = []
+		for i in range(4):
+			offset_x = (i + 1) * gap + i * obstacle_width
+			obstacle = Obstacle(offset_x, self.screen_height - 100)
+			obstacles.append(obstacle)
+		return obstacles

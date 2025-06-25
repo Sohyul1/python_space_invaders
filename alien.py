de3355c62c042e6c630213412356eaedee75abd6
Aeron <dequitoaeron353@gmail.com>
2025-06-26 00:17:21 +0800

    made teh mystery ship

--- alien.py
+++ alien.py
@@ -1,4 +1,4 @@
-import pygame 
+import pygame, random
 
 class Alien(pygame.sprite.Sprite):
     def __init__(self, type, x, y):
@@ -10,4 +10,24 @@ class Alien(pygame.sprite.Sprite):
 
     def update(self, direction):
         self.rect.x += direction
-        
+
+class MysteryShip(pygame.sprite.Sprite):
+	def __init__(self, screen_width):
+		super().__init__()
+		self.screen_width = screen_width
+		self.image = pygame.image.load("Graphics/mystery.png")
+
+		x = random.choice([0, self.screen_width - self.image.get_width()])  
+		if x == 3:
+			self.speed = 3
+		else:
+			self.speed = -3
+			
+		self.rect = self.image.get_rect(topleft = (x, 90))
+
+	def update(self):
+		self.rect.x += self.speed
+		if self.rect.right > self.screen_width :
+			self.kill()
+		elif self.rect.left < 0:
+			self.kill()
